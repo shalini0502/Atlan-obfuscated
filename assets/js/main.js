875c112ef54e9ee8d5dc6a082d3a1e8917ff6b9b
Luan Fernando <luancraft55@gmail.com>
2023-05-21 17:14:21 -0300

    :sparkles: added the nav mobile and added the function to nav menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,25 @@
 /*=============== SHOW MENU ===============*/
-
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+    
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu');
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
 
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
